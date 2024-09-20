3941eab866e9882500e1e669ae24cfff7e905a20
smithdws1 <smithdws@hotmail.com>
2024-09-19 20:51:02 -0600

    Final test successful

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -5,11 +5,11 @@ class Program
     static void Main(string[] args)
     {
         Console.Write("What is your first name? ");
-        string firstName = Console.ReadLine();
+        string first = Console.ReadLine();
 
         Console.Write("What is your last name? ");
-        string lastName = Console.ReadLine();
+        string last = Console.ReadLine();
 
-        Console.WriteLine($"Your name is {lastName}, {firstName} {lastName}.")
+        Console.WriteLine($"Your name is {last}, {first} {last}.");
     }
 }
