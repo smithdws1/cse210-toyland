35888cb3ba19f5eff0402ea6e05783d2d387cf27
smithdws1 <smithdws@hotmail.com>
2024-09-24 21:35:03 -0600

    Final debugging

--- prepare/Learning02/Job.cs
+++ prepare/Learning02/Job.cs
@@ -8,7 +8,6 @@ public class Job
 
     public void Display()
     {
-        Console.WriteLine(${ _jobTitle} ({ _company}) { _startYear}
-        -{ _endYear});
+        Console.WriteLine($"{ _jobTitle} ({ _company}) { _startYear}-{ _endYear}");
     }
 }
