6f2e10466daa864918f0def349a6724454e57412
smithdws1 <smithdws@hotmail.com>
2024-10-14 21:52:34 -0600

    Updating naming conventions.

--- foundation/Foundation1/Program.cs
+++ foundation/Foundation1/Program.cs
@@ -4,10 +4,11 @@ using System.Transactions;
 
 public class Video
 {
+    private List<Comment> _comments = new List<Comment>();
+
     public string Title { get; set; }
     public string Author { get; set; }
     public int LengthSeconds { get; set; }
-    private List<Comment> comments = new List<Comment>();
 
     public Video(string title, string author, int lengthSeconds)
     {
@@ -18,37 +19,37 @@ public class Video
 
     public void AddComment(Comment comment)
     {
-        comments.Add(comment);
+        _comments.Add(comment);
     }
 
     public int GetCommentCount()
     {
-        return comments.Count;
+        return _comments.Count;
     }
 
     public void DisplayVideoInfo()
     {
         Console.WriteLine($"Title: {Title}, Author: {Author}, Length: {LengthSeconds} seconds");
         Console.WriteLine($"Number of Comments: {GetCommentCount()}");
-        foreach (var comment in comments)
+        foreach (var comment in _comments)
         {
             Console.WriteLine($"Comment by {comment.NameCommentator}: {comment.CommentText}");
         }
         Console.WriteLine();
     }
 }
+
 public class Comment
 {
     public string NameCommentator { get; set; }
     public string CommentText { get; set; }
-    
+
     public Comment(string nameCommentator, string commentText)
     {
-            NameCommentator = nameCommentator;
-            CommentText = commentText;
+        NameCommentator = nameCommentator;
+        CommentText = commentText;
     }
 }
-
 
 public class Program
 {
@@ -56,7 +57,7 @@ public class Program
     {
         Video video1 = new Video("First Video Title", "First Author", 10);
         Video video2 = new Video("TBD", "Second Author", 20);
-        Video video3 = new Video("Thrid Thing Here", "Third Author", 30);
+        Video video3 = new Video("Third Thing Here", "Third Author", 30);
 
         video1.AddComment(new Comment("David", "Awesome video!"));
         video1.AddComment(new Comment("Lindsay", "Gross video!"));
--- foundation/Foundation2/Program.cs
+++ foundation/Foundation2/Program.cs
@@ -3,130 +3,131 @@ using System.Collections.Generic;
 
 public class Product
 {
-    private string name;
-    private string productId;
-    private float price;
-    private int quantity;
+    private string _name;
+    private string _productId;
+    private float _price;
+    private int _quantity;
 
     public Product(string name, string productId, float price, int quantity)
     {
-        this.name = name;
-        this.productId = productId;
-        this.price = price;
-        this.quantity = quantity;
+        _name = name;
+        _productId = productId;
+        _price = price;
+        _quantity = quantity;
     }
 
     public string Name
     {
-        get { return name; }
+        get { return _name; }
     }
 
     public string ProductId
     {
-        get { return productId; }
+        get { return _productId; }
     }
 
     public float Price
     {
-        get { return price; }
+        get { return _price; }
     }
 
     public int Quantity
     {
-        get { return quantity; }
+        get { return _quantity; }
     }
 
     public float GetTotalCost()
     {
-        return price * quantity;
+        return _price * _quantity;
     }
 }
 
 public class Address
 {
-    private string street;
-    private string city;
-    private string state;
-    private string zipCode; 
-    private string country;
+    private string _street;
+    private string _city;
+    private string _state;
+    private string _zipCode;
+    private string _country;
+
     public Address(string street, string city, string state, string zipCode, string country)
     {
-        this.street = street;
-        this.city = city;
-        this.state = state;
-        this.zipCode = zipCode;
-        this.country = country;
+        _street = street;
+        _city = city;
+        _state = state;
+        _zipCode = zipCode;
+        _country = country;
     }
 
     public string GetFullAddress()
     {
-        return $"{street}\n{city}, {state} {zipCode}\n{country}";
+        return $"{_street}\n{_city}, {_state} {_zipCode}\n{_country}";
     }
 
     public bool IsInUSA()
     {
-        return country.ToLower() == "usa";
+        return _country.ToLower() == "usa";
     }
 }
 
 public class Customer
 {
-    private string name;
-    private Address address;
+    private string _name;
+    private Address _address;
 
     public Customer(string name, Address address)
     {
-        this.name = name;
-        this.address = address;
+        _name = name;
+        _address = address;
     }
 
     public string Name
     {
-        get { return name; }
+        get { return _name; }
     }
 
     public Address Address
     {
-        get { return address; }
+        get { return _address; }
     }
 
     public bool IsInUSA()
     {
-        return address.IsInUSA();
+        return _address.IsInUSA();
     }
 }
 
 public class Order
 {
-    private List<Product> products = new List<Product>();
-    private Customer customer;
+    private List<Product> _products = new List<Product>();
+    private Customer _customer;
 
     public Order(Customer customer)
     {
-        this.customer = customer;
+        _customer = customer;
     }
 
     public void AddProduct(Product product)
     {
-        products.Add(product);
+        _products.Add(product);
     }
 
     public float GetTotalCost()
     {
         float totalCost = 0;
-        foreach (var product in products)
+        foreach (var product in _products)
         {
             totalCost += product.GetTotalCost();
         }
 
-        float shippingCost = customer.IsInUSA() ? 5f : 35f;
+        float shippingCost = _customer.IsInUSA() ? 5f : 35f;
         return totalCost + shippingCost;
     }
 
     public string GetPackingLabel()
     {
         string packingLabel = "Packing Label:\n";
-        foreach (var product in products)
+        foreach (var product in _products)
         {
             packingLabel += $"{product.Name} (ID: {product.ProductId})\n";
         }
@@ -135,7 +136,7 @@ public class Order
 
     public string GetShippingLabel()
     {
-        return $"Shipping Label:\n{customer.Name}\n{customer.Address.GetFullAddress()}";
+        return $"Shipping Label:\n{_customer.Name}\n{_customer.Address.GetFullAddress()}";
     }
 }
 
