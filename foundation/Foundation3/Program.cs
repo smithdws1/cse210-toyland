46a1a8ecff09ce3dbe9d72933423ff7c604040ac
smithdws1 <smithdws@hotmail.com>
2024-10-28 06:13:49 -0600

    Week 7

--- foundation/Foundation3/Program.cs
+++ foundation/Foundation3/Program.cs
@@ -4,6 +4,16 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Foundation3 World!");
+        var activities = new List<Activity>
+        {
+            new Running(new DateTime(2024, 10, 28), 30, 3.5),
+            new Cycling(new DateTime(2024, 10, 28), 60, 12.6),
+            new Swimming(new DateTime(2024, 10, 28), 90, 50)
+        };
+
+        foreach (var activity in activities)
+        {
+            Console.WriteLine(activity.GetSummary());
+        }
     }
 }
---
+++ foundation/Foundation3/Swimming.cs
@@ -0,0 +1,26 @@
+public class Swimming : Activity
+{
+    private int _laps;
+    private const double LapsToMiles = 50 / 1000.0 * 0.62;
+
+    public Swimming(DateTime date, int minutes, int laps)
+        : base(date, minutes)
+    {
+        _laps = laps;
+    }
+
+    public override double GetDistance()
+    {
+        return _laps * LapsToMiles;
+    }
+
+    public override double GetSpeed()
+    {
+        return (GetDistance() / Minutes) * 60;
+    }
+
+    public override double GetPace()
+    {
+        return Minutes / GetDistance();
+    }
+}
